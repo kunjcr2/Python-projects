2f45e614038d4aa30551ba7e1ca673947b79af65
Kunj Shah <159096079+kunjcr2@users.noreply.github.com>
2024-08-06 16:03:00 +0530

    Update QRCode.py

--- QRCode.py
+++ QRCode.py
@@ -4,14 +4,14 @@ import qrcode.constants
 
 # Alternate way and easier one
 
-# img = qr.make("https://www.instagram.com/kunj.cr2/")
-# img.save("ig@kunj.cr2.png")
+# img = qr.make("URL goes here")
+# img.save("name of the image goes here")
 
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_H,
                    box_size=10,border=4,)
-qr.add_data("https://www.instagram.com/kunj.cr2/")
+qr.add_data("URL goes here")
 qr.make(fit=True)
 
 img = qr.make_image()
-img.save("ig@kunj.cr2.png")
+img.save("name of the image goes here")
